6c15ee9b42c348915b68189b1e0b3b84bdd94a6e
Dries <driescroons@gmail.com>
2017-02-17 15:34:31 +0100

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,44 @@
+
+// const MongoClient = require('mongodb').MongoClient;
+// const {MongoClient} = require('mongodb'); // <- object destructering
+const {MongoClient, ObjectID} = require('mongodb'); // <- kunnen er meerdere maken nu
+
+// // object destructering
+// // let's u pull out properties from an object, creating variables
+// var user = {name: 'Dries', age: 22}
+// // we can pull these out and put these in a variables
+// var {name} = user;
+// console.log(name); // -> Dries
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  // return console.log works fine and just stops.. js... hehe
+  if (err) return console.log('Woops, something went wrong connecting to the database server!');
+
+  console.log('major tom to ground mongodb');
+
+  // insert new doc into Users collection (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Dries',
+  //   age: 22,
+  //   location: 'Belgium'
+  // }, (err, result) => {
+  //   if (err) return console.log("woops-daisy, something went wrong inserting that shit");
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   // passing err aswell to display object
+  //   if (err) return console.log('Wooooops, unable to insert todo', err);
+  //
+  //   // result contains document that was inserted.
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,53 @@
+
+// const MongoClient = require('mongodb').MongoClient;
+// const {MongoClient} = require('mongodb'); // <- object destructering
+const {MongoClient, ObjectID} = require('mongodb'); // <- kunnen er meerdere maken nu
+
+// // object destructering
+// // let's u pull out properties from an object, creating variables
+// var user = {name: 'Dries', age: 22}
+// // we can pull these out and put these in a variables
+// var {name} = user;
+// console.log(name); // -> Dries
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  // return console.log works fine and just stops.. js... hehe
+  if (err) return console.log('Woops, something went wrong connecting to the database server!');
+
+  console.log('major tom to ground mongodb');
+
+  // db.collection('Todos').find().toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch todo's", err);
+  // });
+
+  // db.collection('Todos').find({completed: true}).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch todo's", err);
+  // });
+
+  // db.collection('Todos').find({_id: new ObjectID("58a705db67e2b6c5a691fd39")}).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch todo's", err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(JSON.stringify(count, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch todo's", err);
+  // });
+
+  db.collection('Users').find({name: 'Dries'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log("dayum dawg", err);
+  });
+
+  // db.close();
+});
